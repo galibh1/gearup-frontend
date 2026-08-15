6a8bc030223c0f8db1aa338761a1e8705a036f6d
galibh1 <galibh233@gmail.com>
2026-08-15 21:00:08 +0600

    project deployment test

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -1,11 +1,3 @@
-import { Calendar } from "@/components/ui/calendar";
-import { Button } from "@/components/ui/button";
-
 export default function Page() {
-  return (
-    <div className="p-6 border-2 rounded-md">
-      <Calendar />
-      <Button>Click me</Button>
-    </div>
-  );
+  return <div>Login Page</div>;
 }
