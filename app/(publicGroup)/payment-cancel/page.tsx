3d990d3348d25065edc7b729a5e6d16877d1fa5e
galibh1 <galibh233@gmail.com>
2026-08-27 22:21:24 +0600

    payment change

--- app/(publicGroup)/payment-cancel/page.tsx
+++ app/(publicGroup)/payment-cancel/page.tsx
@@ -1,12 +1,14 @@
 export default function PaymentCancelPage(){
 
+  
+
   return (
     <div className="min-h-screen flex items-center justify-center">
 
       <div className="p-10 rounded-xl shadow text-center">
 
         <h1 className="text-3xl font-bold text-red-600">
-          Payment Cancelled ❌
+          Payment Cancelled  ❌
         </h1>
 
         <p className="mt-4">
